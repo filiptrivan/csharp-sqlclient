d05fcea8f9c557331f18051c29f75e7bb06dc5d6
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2025-07-28 18:35:04 +0200

    Dummy C# tests

---
+++ Dummy/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Dummy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float x = 1;
+
+            double a = x;
+            //long b = x;
+            //bool c = x;
+            double? d = x;
+            float? e = x;
+        }
+    }
+}
